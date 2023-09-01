19842edc0231b9b18a6d7a90107a405cbab54838
Rail <railbolo@gmail.com>
2023-09-01 22:35:17 +0400

    end hw2

--- lesson_2/bankomat.py
+++ lesson_2/bankomat.py
@@ -1,3 +1,5 @@
+import os
+
 # Напишите программу банкомат.
 # ✔ Начальная сумма равна нулю
 # ✔ Допустимые действия: пополнить, снять, выйти
@@ -8,3 +10,54 @@
 # ✔ При превышении суммы в 5 млн, вычитать налог на богатство 10% перед каждой
 # операцией, даже ошибочной
 # ✔ Любое действие выводит сумму денег
+
+DIVIDER = 50
+TAKING_OFF = 0.015
+ACCRUAL = 0.03
+TAX = 0.1
+
+amount = 0
+
+def addition():
+    pass
+def removal():
+    pass
+def show():
+    pass
+def menu():
+    os.system("cls")
+    menu = ('Добро пожаловать в программу "Банкомат"\n\n'
+            'Доступные действия:\n'
+            '1 - Пополнить счет\n'
+            '2 - Снять наличные\n'
+            '3 - Показать баланс\n'
+            '0 - Выход')
+    print(menu)
+    fail_answer = 5
+    while fail_answer > 0:
+        answer = input('Введите номер действия:>> ')
+        if not answer.isdigit():
+            print('Нужно ввести число от 1 до 3')
+            fail_answer -= 1
+            continue
+        else:
+            if answer == '1':
+                os.system("cls")
+                addition()
+                print(menu)
+            if answer == '2':
+                os.system("cls")
+                removal()
+                print(menu)
+            if answer == '3':
+                os.system("cls")
+                show()
+                print(menu)
+            if answer == '0':
+                exit(0)
+
+    print('Похоже, вы делаете что-то не так((')
+    exit(0)
+if __name__ == '__main__':
+    menu()
+
