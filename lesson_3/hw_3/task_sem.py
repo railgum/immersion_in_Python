b4e29fc7a71ad43fd6a97cf8bb545101e121462e
Rail <railbolo@gmail.com>
2023-09-07 04:56:55 +0400

    end hw3

--- lesson_3/hw_3/task_sem.py
+++ lesson_3/hw_3/task_sem.py
@@ -10,15 +10,27 @@
 #   на любое большее количество друзей.
 
 hike = {
-    'misha': ('tent', 'pot', 'matches', 'axe', 'rope', 'canned food'),
-    'roma': ('knife', 'salt', 'rope', 'sweater', 'canned food', 'tent'),
-    'sasha': ('rice', 'noodles', 'matches', 'salt', 'tent'),
+    'misha': ('tent', 'pot', 'matches', 'axe', 'rope', 'canned food', 'knife'),
+    'roma': ('knife', 'salt', 'rope', 'sweater', 'canned food'),
+    'sasha': ('noodles', 'matches', 'salt', 'tent', 'axe'),
 }
 
 things = set()
 for key, values in hike.items():
     for i in values:
         things.add(i)
-
-
 print(f'Взяли следующие вещи: {things}')
+
+all_things = list(hike.values())
+uniq_things = set()
+for i in all_things:
+    uniq_things = set(i).symmetric_difference(uniq_things)
+print(f'Уникальные вещи: {uniq_things}')
+
+friend_not_thing = {}
+for key, value in hike.items():
+    friend_not_thing[key] = uniq_things.difference(set(value))
+
+for key, value in friend_not_thing.items():
+    print(f'У {key} нет {value}')
+
