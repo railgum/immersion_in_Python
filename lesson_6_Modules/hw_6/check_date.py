fda05322dd9e3a5c8bcd612e7e2342578af62557
Rail <railbolo@gmail.com>
2023-09-17 12:47:57 +0400

    end task2 hw6

--- lesson_6_Modules/hw_6/check_date.py
+++ lesson_6_Modules/hw_6/check_date.py
@@ -1,4 +1,3 @@
-import sys
 
 def __check_leap_year(year: int):
     if year % 4 != 0:
@@ -12,37 +11,29 @@ def __check_leap_year(year: int):
         return True
 
 
-def check_date(*date_user):
+def check_date(date: str = '01.01.0001'):
     MIN_YEAR = 1
     MAX_YEAR = 9999
     month_30 = [4, 6, 9, 11]
-
-    if date_user:
-        usr_date = str(list(*date_user))
-        print(usr_date.split('.'))
+    usr_date = list(map(int, date.split('.')))
+    print(date.split('.'))
+    if len(str(usr_date[2])) != 4 \
+            or len(str(usr_date[0])) != 2 \
+            or len(str(usr_date[1])) != 2:
+        return f'{date} - Неверный формат даты'
     else:
-        while True:
-            date = input('Введите дату в формате DD.MM.YYYY: ').split('.')
-            print(date)
-            if (len(date[0]) or len(date[1])) != 2 or len(date[2]) != 4:
-                print('Неверный формат даты')
-            else:
-                usr_date = list(map(int, date))
-                break
+        month_febr = 29 if __check_leap_year(usr_date[2]) else 28
+        if (usr_date[2] > MAX_YEAR or usr_date[2] < MIN_YEAR) \
+                or (usr_date[0] > 31 or usr_date[0] < 1) \
+                or (usr_date[1] > 12 or usr_date[1] < 1):
+            return f'{date} - Дата не существует'
+        if usr_date[1] == 2 and usr_date[0] > month_febr:
+            return f'{date} - Дата не существует'
+        if usr_date[1] in month_30 and usr_date[0] > 30:
+            return f'{date} - Дата не существует'
+        if month_febr == 28:
+            return f'{date} - Дата существует'
+        else:
+            return f'{date} - Дата существует и год високосный'
 
-    month_febr = 29 if __check_leap_year(usr_date[2]) else 28
-    if (usr_date[2] > MAX_YEAR or usr_date[2] < MIN_YEAR) \
-            or (usr_date[0] > 31 or usr_date[0] < 1) \
-            or (usr_date[1] > 12 or usr_date[1] < 1):
-        return f'Дата не существует'
-    if usr_date[1] == 2 and usr_date[0] > month_febr:
-        return f'Дата не существует'
-    if usr_date[1] in month_30 and usr_date[0] > 30:
-        return f'Дата не существует'
-    if month_febr == 28:
-        return f'Дата существует'
-    else:
-        return f'Дата существует и год високосный'
-
-
-print(check_date('12.15.2015'))
+# print(check_date('12.12.2012'))
---
+++ lesson_6_Modules/hw_6/check_date_terminal.py
@@ -0,0 +1,6 @@
+from check_date import check_date
+import sys
+
+if __name__ == '__main__':
+    options = sys.argv[1]
+    print(check_date(options))
