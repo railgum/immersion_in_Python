fc07a38299d7bb381e8dcc2cba5113906208ac22
Rail <railbolo@gmail.com>
2023-09-29 20:30:37 +0400

    start hw_9

---
+++ lesson_9_Decorators/sem_9/task_1.py
@@ -0,0 +1,35 @@
+# Создайте функцию-замыкание, которая запрашивает два целых
+# числа:
+# ○ от 1 до 100 для загадывания,
+# ○ от 1 до 10 для количества попыток
+# Функция возвращает функцию, которая через консоль просит
+# угадать загаданное число за указанное число попыток.
+import random
+
+
+def outer():
+    a = random.randint(1, 100)
+    b = random.randint(1, 10)
+
+    def inner():
+        nonlocal a, b
+        print(f'У тебя {b} попыток...')
+        while b:
+            quess = int(input('Введите число: '))
+            if quess > a:
+                print(f'Число меньше, чем {quess}')
+            elif quess < a:
+                print(f'Число больше, чем {quess}')
+            else:
+                print(f'Угадал за  {b}! Это число - {a}')
+                break
+            b -= 1
+        else:
+            print(f'Сорян, попытки закончились :(')
+
+    return inner
+
+
+example = outer()
+example()
+# print(globals())
---
+++ lesson_9_Decorators/sem_9/task_2.py
@@ -0,0 +1,39 @@
+# Дорабатываем задачу 1.
+# Превратите внешнюю функцию в декоратор.
+# Она должна проверять, входят ли переданные в функцию-угадайку числа
+# в диапазоны [1, 100] и [1, 10].
+# Если не входят, вызывать функцию со случайными числами
+# из диапазонов.
+import random
+from typing import Callable
+
+
+def deco(func):
+    def wrapper(*args, **kwargs):
+        a, b, *_ = args
+        if a not in range(1, 101):
+            print(f'Недопустимый диапазон')
+            a = random.randint(1, 100)
+        if b not in range(1, 11):
+            print(f'Недопустимый диапазон')
+            b = random.randint(1, 10)
+        func(a, b)
+
+    return wrapper()
+
+
+@deco
+def inner(a, b):
+    print(f'У тебя {b} попыток...')
+    while b:
+        quess = int(input('Введите число: '))
+        if quess > a:
+            print(f'Число меньше, чем {quess}')
+        elif quess < a:
+            print(f'Число больше, чем {quess}')
+        else:
+            print(f'Угадал за  {b}! Это число - {a}')
+            break
+        b -= 1
+    else:
+        print(f'Сорян, попытки закончились :(')
